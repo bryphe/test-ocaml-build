2200f25659f55bb590863a9cfefb0bc2b72ed977
Bryan Phelps <bryphe@outlook.com>
2018-08-31 16:56:34 -0700

    Handle newlines

--- build.bat
+++ build.bat
@@ -3,5 +3,7 @@ git clone https://github.com/esy-ocaml/ocaml
 cd ocaml
 git checkout 4.6.0+esy
 
+crlf --set=LF *.sh
+
 esy-bash ./esy-configure -no-cfi -prefix C:/projects/ocaml-prefix
 esy-bash ./esy-build
