a96b8df4c5f9fb3ba2753f23ba2fb95d59a3908d
Bryan Phelps <bryphe@outlook.com>
2018-08-31 17:08:27 -0700

    test

--- build.bat
+++ build.bat
@@ -1,9 +1,9 @@
-echo starting build
+echo prepping repo
 git clone https://github.com/esy-ocaml/ocaml
 cd ocaml
 git checkout 4.6.0+esy
-
 crlf --set=LF *.sh
+echo done prepping
 
 esy-bash ./esy-configure -no-cfi -prefix C:/projects/ocaml-prefix
 esy-bash ./esy-build
---
+++ build2.bat
@@ -0,0 +1,5 @@
+echo building
+cd ocaml
+esy-bash ./esy-configure -no-cfi -prefix C:/projects/ocaml-prefix
+esy-bash ./esy-build
+echo done
